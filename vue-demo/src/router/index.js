10251d6b9a2dfffa0b03168069f4bc9b5f96edd8
kyle-233 <kyle07142113@163.com>
2020-07-17 19:07:45 +0800

    构建项目、配置样式、路由、elementui

---
+++ vue-demo/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "login" //重定向
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login/Login.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ vue-demo/src/views/Login/Login.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="login">
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    components: {
+
+    },
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+    watch: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
